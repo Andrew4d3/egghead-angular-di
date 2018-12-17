6e99f791d97db0643d4c497334dcea41b47f215d
Andrew4d3 <andrew4d3@gmail.com>
2018-12-17 19:47:43 -0300

    Lesson 5: Use Angular Factory Providers with dependencies

--- src/app/list/list.component.ts
+++ src/app/list/list.component.ts
@@ -2,14 +2,20 @@ import { Component, OnInit } from "@angular/core";
 import { DataService } from "../data.service";
 import { OtherDataService } from "../other-data.service";
 import { LogDebugger } from "../log-debugger.service";
+import { ConsoleService } from "../console.service";
 
 @Component({
   selector: "app-list",
   templateUrl: "./list.component.html",
   styleUrls: ["./list.component.css"],
   providers: [
+    ConsoleService,
     { provide: DataService, useClass: OtherDataService },
-    { provide: LogDebugger, useFactory: () => new LogDebugger(true) }
+    {
+      provide: LogDebugger,
+      useFactory: consoleService => new LogDebugger(consoleService, true),
+      deps: [ConsoleService]
+    }
   ]
 })
 export class ListComponent implements OnInit {
--- src/app/log-debugger.service.ts
+++ src/app/log-debugger.service.ts
@@ -1,5 +1,10 @@
+import { ConsoleService } from "./console.service";
+
 export class LogDebugger {
-  constructor(private enabled: boolean) {}
+  constructor(
+    private consoleService: ConsoleService,
+    private enabled: boolean
+  ) {}
 
   debug(message) {
     if (this.enabled) {
