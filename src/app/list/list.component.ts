b033b476bc5fa1dd1d36904a6f0a925e11ef8a2b
Andrew4d3 <andrew4d3@gmail.com>
2018-12-17 19:34:43 -0300

    Lesson 4: Use Angular Factory Providers

--- src/app/list/list.component.ts
+++ src/app/list/list.component.ts
@@ -1,19 +1,27 @@
 import { Component, OnInit } from "@angular/core";
 import { DataService } from "../data.service";
 import { OtherDataService } from "../other-data.service";
+import { LogDebugger } from "../log-debugger.service";
 
 @Component({
   selector: "app-list",
   templateUrl: "./list.component.html",
   styleUrls: ["./list.component.css"],
-  providers: [{ provide: DataService, useClass: OtherDataService }]
+  providers: [
+    { provide: DataService, useClass: OtherDataService },
+    { provide: LogDebugger, useFactory: () => new LogDebugger(true) }
+  ]
 })
 export class ListComponent implements OnInit {
   items: Array<any>;
 
-  constructor(private dataService: DataService) {}
+  constructor(
+    private dataService: DataService,
+    private logDebugger: LogDebugger
+  ) {}
 
   ngOnInit() {
+    this.logDebugger.debug("Getting items ...");
     this.items = this.dataService.getItems();
   }
 }
